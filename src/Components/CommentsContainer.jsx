8c689ff54892b6ace0394b2b2724cf6924f5bc23
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-08-07 22:14:20 +0530

    feat : Nested Comments

---
+++ src/Components/CommentsContainer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+export default function CommentsContainer() {
+	const comments = [
+		{
+			name: "Aryan",
+			text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nulla, voluptates minus vero doloribus laboriosam.",
+			replies: [
+				{
+					name: "Aryan",
+					text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nulla, voluptates minus vero doloribus laboriosam.",
+					replies: [],
+				},
+			],
+		},
+		{
+			name: "Aryan",
+			text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nulla, voluptates minus vero doloribus laboriosam.",
+			replies: [],
+		},
+		{
+			name: "Aryan",
+			text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nulla, voluptates minus vero doloribus laboriosam.",
+			replies: [
+				{
+					name: "Aryan",
+					text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nulla, voluptates minus vero doloribus laboriosam.",
+					replies: [
+						{
+							name: "Aryan",
+							text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nulla, voluptates minus vero doloribus laboriosam.",
+							replies: [
+								{
+									name: "Aryan",
+									text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nulla, voluptates minus vero doloribus laboriosam.",
+									replies: [],
+								},
+							],
+						},
+					],
+				},
+			],
+		},
+		{
+			name: "Aryan",
+			text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nulla, voluptates minus vero doloribus laboriosam.",
+			replies: [],
+		},
+	];
+
+	return <div>Comments</div>;
+}
